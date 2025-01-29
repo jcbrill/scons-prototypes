f33d182cff2a026c867687db2078088420eed210
Joseph Brill <48932340+jcbrill@users.noreply.github.com>
2025-01-29 08:57:56 -0500

    scons-issue-4656: * simplify headers for MultipleTest

--- scons/scons-issue-4656/tests/MultipleTest/include/include1.h
+++ scons/scons-issue-4656/tests/MultipleTest/include/include1.h
@@ -1,2 +1,13 @@
-/* #pragma message("include2.h") */
+#ifndef INCLUDE1_H
+#define INCLUDE1_H
+/* #pragma message("include1.h") */
+
+#define PATH_1
 #include "include2.h"
+#undef PATH_1
+
+#define PATH_2
+#include "include2.h"
+#undef PATH_2
+
+#endif
